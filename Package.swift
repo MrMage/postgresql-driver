09345040fcf9e2a5652e00a357e381d83db1bdb0
Daniel Alm <Daniel.Alm@ForumD.net>
2017-03-23 16:47:15 +0100

    Switch to my own PostgreSQL package.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentPostgreSQL",
     dependencies: [
-        .Package(url: "https://github.com/vapor/postgresql.git", majorVersion: 1),
+        .Package(url: "https://github.com/MrMage/postgresql.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 1)
     ]
 )
